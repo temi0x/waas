2033a0188b61fd3daa265deaed9f8b8f2e256ecf
Temiloluwa Adelowo <temiadev@gmail.com>
2024-07-14 12:14:23 +0100

    Update log.go

--- internal/tools/analytics/log.go
+++ internal/tools/analytics/log.go
@@ -30,7 +30,7 @@ func LogEvent(event string, data string) error {
 }
 
 func StoreTransaction(txn TransactionLog) error {
-	err := database.StoreTxnInDb(txn.WalletAddress, txn.TransactionID, txn.TargetAddress, txn.TokenType, txn.Amount, "success", "")
+	err := database.StoreTxnInDb(txn.WalletAddress, txn.TransactionID, txn.TargetAddress, txn.TokenType, txn.Amount, txn.Status, txn.ErrorMessage)
 	if err != nil {
 		log.Printf("Error storing transaction: %v", err)
 		return fmt.Errorf("error storing transaction: %v", err)
